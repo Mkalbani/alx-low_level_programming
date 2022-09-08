9f8c74470f1852500a1caadcd67bbfaa27d6fba7
mkothm <mkothman.jr@gmail.com>
2022-09-08 13:56:32 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,12 @@
-#!/bin/bash
-put ("Programming is like building a multilingual puzzle");
-    return (0);
+#include <stdio.h>
+
+/**
+ * Main -Serves as the entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int (void)
+{
+	put ("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
