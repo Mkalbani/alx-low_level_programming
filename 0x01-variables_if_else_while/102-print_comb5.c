b554c63d142e1e9ea80f9d123d624eca81e5da29
mkothm <mkothman.jr@gmail.com>
2022-09-10 13:18:12 -0700

    new

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 99; a++)
+	{
+		for (b = a + 1; b <= 99; b++)
+		{
+			putchar('0' + a / 10);
+			putchar('0' + a % 10);
+
+			putchar(' ');
+
+			putchar('0' + b / 10);
+			putchar('0' + b % 10)
+
+			if (a == 98 && b == 99)
+			{
+				break;
+			}
+			putchar(',')
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
