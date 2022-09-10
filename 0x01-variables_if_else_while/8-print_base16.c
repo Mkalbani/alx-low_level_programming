a896bb04519d60ab23b93ecbf8ad0cc4d358e21f
mkothm <mkothman.jr@gmail.com>
2022-09-10 10:41:28 -0700

    done

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	char ch[] = "0123456789abcdef";
+
+	for (i = '0'; i < '16'; i++)
+	{
+		putchar(ch[i]);
+	}
+	putchar('\n');
+
+	return (0);
+}
