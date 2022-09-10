0a06616c6f369804e328f7f5bccb1047b1af807b
mkothm <mkothman.jr@gmail.com>
2022-09-10 09:30:20 -0700

    all single digits starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Description: This program will all single
+ * digit numbers of base 10 starting from 0
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+
+i = '0';
+for (i = 0, i < 10; i++)
+	printf("%d, i");
+	printf("\n");
+return (0);
+}
