fb49eb98fdb96d662783b0af6f6516e5c0db80a4
mkothm <mkothman.jr@gmail.com>
2022-09-10 11:51:51 -0700

    two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n, a = '0', b;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		for (b = a; b <= '9'; b++);
+		{
+			if (n != b)
+			{	
+				putchar(n);
+				putchar(b);
+			}
+			if (n == )
+			{
+				continue;
+			]
+			if (n == '8' && b == '9')
+			{
+				break;
+			}
+			else
+			{
+			putchar(',');
+			putchar(' ');
+			}
+		}
+		a++
+	}
+	putchar('\n');
+	return (0);
+}
