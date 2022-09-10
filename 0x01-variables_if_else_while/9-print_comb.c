4927f476494a36bf093bd1cf9f2f3427bee6173e
mkothm <mkothman.jr@gmail.com>
2022-09-10 11:28:09 -0700

    new

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(num);
+		if (n == '9')
+		{
+		break;
+		}
+		else
+			putchar(',');
+			putchar(' ')
+	}
+		putchar('\n');
+		return (0);
+}
