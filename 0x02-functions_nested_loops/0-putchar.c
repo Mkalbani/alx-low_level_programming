f463dd1049e5214b9ef8c3fa02df1629e978e32f
mkothm <mkothman.jr@gmail.com>
2022-09-13 09:12:25 -0700

    print underscored word

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - entry point
- *
- * Return: 0 
- */
+* main - entry point
+*
+* Return: 0
+*/
 
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
-	return (0);
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
 }
