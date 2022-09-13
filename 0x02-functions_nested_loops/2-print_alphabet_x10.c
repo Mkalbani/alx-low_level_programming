6bff4260b8eea5ce0f859346219177d6049ca7a7
mkothm <mkothman.jr@gmail.com>
2022-09-13 13:39:41 -0700

    done

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,10 +9,12 @@ void print_alphabet_x10(void)
 {
 	char c, i;
 	
-	for (c = 'a'; c <= 'z'; c++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (i = 0; i <= 10; i++)
-		_putchar(c);
+		for (c = 'a'; c <= 'z'; c++)
+		{	
+			_putchar(c);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
