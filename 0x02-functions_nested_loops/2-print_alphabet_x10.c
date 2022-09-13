2ca0ee01351e5d71b92a6906b61e8706f4e4d3f4
mkothm <mkothman.jr@gmail.com>
2022-09-13 13:20:37 -0700

    done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	
+	for (c = 'a'; c <= 'z'; c x '10')
+		_putchar(c)
+	_putchar('\n');
+	return (0);
+}
