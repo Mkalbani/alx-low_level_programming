3ce5e210327f25798961bb175534e005968e09b0
mkothm <mkothman.jr@gmail.com>
2022-09-14 16:42:10 -0700

    done

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,14 +2,14 @@
 
 /**
  * jack_bauer - check code
- * 
+ *
  * Return: 0
  */
 
 void jack_bauer(void)
 {
 	int a, b, c, d;
-	
+
 	for (a = 0; a <= 2; a++)
 	{
 		for (b = 0; b <= 9; b++)
@@ -17,10 +17,10 @@ void jack_bauer(void)
 			for (c = 0; c <= 5; c++)
 			{
 				for (d = 0; d <= 9; d++)
-				{	
+				{
 					if (a == 2 && b == 3)
 					{break;
-					}	
+					}
 					_putchar(a + '0');
 					_putchar(b + '0');
 					_putchar(':');
