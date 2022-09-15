148863dc05576acfce9983245218aa8c755aa3ae
mkothm <mkothman.jr@gmail.com>
2022-09-15 11:12:19 -0700

    new

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,33 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Description: This program will assign a random number
+ *
+ * to the variable n each time it is executed
+ * Return: 0
+ */
+
+void positive_or_negative(int i)
+{
+	int n;
+
+	srand(time(0));
+	n = i
+
+	if (n > 0)
+			printf("%d is positive\n"
+					, n);
+	else if (n == 0)
+			printf("%d is zero\n"
+					, n);
+	else
+			printf("%d is negative\n", n);
+
+	return (0);
+}
 
 /**
 * main - tests function that prints if integer is positive or negative
