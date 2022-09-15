632ddc4d90e0895c0a74e543b37cc2d3ede5ef76
mkothm <mkothman.jr@gmail.com>
2022-09-15 06:53:48 -0700

    done

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - check the code.
+ * 
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int a;
+	
+	for (a = '0'; a <= '9'; a++)
+	{
+		_putchar(a);
+		_putchar('\n');
+	}
+}
