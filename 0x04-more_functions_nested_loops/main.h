2841020a640dcdf20737baa9fd45e798a6762252
mkothm <mkothman.jr@gmail.com>
2022-09-15 09:15:24 -0700

    new

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,6 +9,7 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_square(int size);
 void print_triangle(int size);
+void print_number(int n);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
