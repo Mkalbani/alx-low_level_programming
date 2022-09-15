aea945ae2e128f6417e8f4fe1908090423d51a1e
mkothm <mkothman.jr@gmail.com>
2022-09-15 06:24:51 -0700

    done

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - check the code.
+ * @a: multiplies integers
+ * @b: mulitplies integers
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	printf(%d\n: mul(a, b);
+	return (0);
+}
