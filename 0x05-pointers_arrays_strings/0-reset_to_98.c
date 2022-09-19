c04460bfc7d41d1feefba957f74b0e4c940750df
mkothm <mkothman.jr@gmail.com>
2022-09-19 07:04:13 -0700

    resetting value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * reset_to_98 -check if codr works.
+ * @*n: value reset.
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	int n = 98
+	int *p = n
+
+	putchar(&n);
+
+return (0);
+}
