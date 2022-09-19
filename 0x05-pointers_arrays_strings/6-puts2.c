dac449edf096ad88daa4c499ae62648fd42e5d7a
mkothm <mkothman.jr@gmail.com>
2022-09-19 15:46:27 -0700

    done

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a strin
+ * @str: string parameter.
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (i >= 0)
+	{
+		if (str[i] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+}
