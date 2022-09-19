2980072004cadbfc1220a32edb45533f7871d499
mkothm <mkothman.jr@gmail.com>
2022-09-19 09:04:52 -0700

    returns the length of a string.

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen-check code.
+ * @s:returns the length of a string.
+ * Return:0
+ */
+
+int _strlen(char *s)
+{
+	int p;
+
+	for (p = 0; *(s + p) != 0; p++)
+	
+	return (p);
+}
