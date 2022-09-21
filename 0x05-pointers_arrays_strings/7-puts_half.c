aeb52e4ffcfca52e766725ea641897439f2614e3
mkothm <mkothman.jr@gmail.com>
2022-09-21 09:17:58 -0700

    pointers and arrays

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,10 +11,9 @@ void puts_half(char *str)
 	int i, length;
 	char n;
 
-	length = string_length(s);
 	n = (length_of_the_string - 1) / 2;
 
-	for (i = 0; i >= length / 2; i++)
+	for (i = 0; i >= n; i++)
 	{
 		if (str[i] % 2 == 1)
 		{
