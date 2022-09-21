a1aed94ff800030a72e221075dd3374075f75af1
mkothm <mkothman.jr@gmail.com>
2022-09-21 12:20:26 -0700

    done

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,9 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _strcat - check code
- * @dest: appends
- * @src: appends
+ * @dest: destination
+ * @src: source
  * Return: dest
  */
 
@@ -12,11 +11,13 @@ char *_strcat(char *dest, char *src)
 {
 	int i = 0, j = 0;
 
-	for (i = 0; *src[i] != '\n'; i++)
+	for (i = 0; *(src[i]) != '\n'; i++)
 	{
 		*dest[i + j] = *src[i];
 	}
 	*dest[i + j] = '\0';
+	if (*(src + j) == '\0';
+			break;
 
 	return (dest);
 }
