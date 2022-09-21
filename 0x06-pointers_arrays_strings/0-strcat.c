85a0a818f799c0dc62ee733ce778bc87b62686d1
mkothm <mkothman.jr@gmail.com>
2022-09-21 12:43:32 -0700

    new

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,12 +9,13 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int i = 0, j = 0;
 
-	for (i = 0; *(dest + i) != '\n'; i++)
-		for (j = 0; j >= 0; j++)
-			*(dest + i) = *(src + j);
-			if (*(src + j) == '\0')
-				break;
+	for (i = 0; *(dest + i) != '\0'; i++)
+		
+	for (j = 0; j >= 0; j++)
+		*(dest + i) = *(src + j);
+		if (*(src + j) == '\0')
+			break;
 	return (dest);
 }
