b1c88311514c5f06b4ca2e7445657cc1b3a9ce37
mkothm <mkothman.jr@gmail.com>
2022-09-22 15:53:42 -0700

    jew

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,6 +5,8 @@
  * Return: the pointer to dest.
  */
 
+char *cap_string(char *)
+{
 int i;
 
 	for (i = 0; str[i] != '\0'; i++)
@@ -28,7 +30,7 @@ int i;
 		}
 		else
 		{
-			if (str[i] >= 65 && str[i] <= 90)
+			if (str[i] >= 'A' && str[i] <= 'Z')
 			{
 				str[i] = str[i] + 32;
 			}
