55f6e6a3e9cfac72daa9f89c615142c8efb3fccd
mkothm <mkothman.jr@gmail.com>
2022-09-22 14:48:48 -0700

    done

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,13 +8,16 @@
  */
 
 char *string_toupper(char *s)
-{   int c = 0;
-   
-   while (s[c] != '\0') {
-      if (s[c] >= 'a' && s[c] <= 'z') {
-         s[c] = s[c] - 32;
-      }
-      c++;
-   }
-    return (s);
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+		if (s[c] >= 'a' && s[c] <= 'z')
+		{
+			s[c] = s[c] - 32;
+		}
+		c++;
+	}
+	return (s);
 }
