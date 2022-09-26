8020d73765fdefe1517c5995238064c2d6777d06
mkothm <mkothman.jr@gmail.com>
2022-09-26 10:16:52 -0700

    locating c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strchr - check code
+ * @s: string
+ * @c: character
+ * Return: pointer to the first occurrence of the character c in the string s, or NULL if the character is not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	if (c != NULL)
+		_putchar(c)
+	else
+
+	return (NULL);
+}
