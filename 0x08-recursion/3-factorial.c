5acc1a64dd635a50a75e64e620d0f145d0d0ee7a
mkothm <mkothman.jr@gmail.com>
2022-09-28 11:13:50 -0700

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -4,16 +4,16 @@
  * factorial - check code
  * @n: number.
  *
- * Return: -1 if n lower than 0, else  n 
+ * Return: -1 if n lower than 0, else  n
  */
 
 int factorial(int n)
 {
 	if (n == 0)
 		return (1);
-	else if (n < 0);
+	else if (n < 0)
 		return (-1);
 	else
-	return n * factorial(n - 1);
+		return (n * factorial(n - 1));
 
 }
