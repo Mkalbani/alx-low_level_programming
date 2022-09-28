77c3e06ed81e993d64bcfdf39ea79fa797971182
mkothm <mkothman.jr@gmail.com>
2022-09-28 10:16:07 -0700

    strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - check code
+ * @s: string
+ * Return: len
+ */
+
+int _strlen_recursion(char *s)
+{
+	int len;
+
+	if (*s != '\0')
+	{
+		return (1 +_strlen_recursion(s + 1);
+	}
+	return (len);
+
+}
