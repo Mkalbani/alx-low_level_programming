93cbe57ad5702809d01d39e067552a2e00e7427d
mkothm <mkothman.jr@gmail.com>
2022-10-01 15:45:52 -0700

    new

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _abs-prints the absolute value of an integer.
+ * @a:the integer to compute its absolute value.
+ * Return:returns the abs value.
+ *
+ */
+
+int _abs(int a)
+{
+}
