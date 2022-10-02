9c8de0753ec4cdce0134a086de1e2433425fe069
mkothm <mkothman.jr@gmail.com>
2022-10-02 12:22:31 -0700

    a program that prints utsown name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - progrm to prints argument
+ *@argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc __attribute((unused)), char **argv)
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
