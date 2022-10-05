224bc9dca50eb450c5c306efa539ed9c5f25890d
mkothm <mkothman.jr@gmail.com>
2022-10-05 07:19:04 -0700

    nee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (0);
 
-	str = malloc(sizeof(c) * size);
+	str = malloc(sizeof(c) * (size + 1));
 
 	if (str == NULL)
 		return (NULL);
---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - returns a pointer to a newly allocated space in memory.
+ * @s1: string.
+ * @s2: string.
+ *
+ * Return: pointer
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	
+}
