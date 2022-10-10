333243ce3f85a0479a7147ab1db3f52e9ef0ccfd
mkothm <mkothman.jr@gmail.com>
2022-10-10 10:58:44 -0700

    filename

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,6 +1,6 @@
 #ifndef _PI_
 #define _PI_
 
-#define PI 3.1415926535
+#define __PI__ 3.1415926535
 
 #endif
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/** main -entry point
+* 
+* Return: 0
+*/
+
+int main(void)
+{
+	printf("Print New File %s\n", __FILE__);
+
+	return (0);
+}
