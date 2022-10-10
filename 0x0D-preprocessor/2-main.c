89f5819af075caad4c9f004ab21ba5b075f740ec
mkothm <mkothman.jr@gmail.com>
2022-10-10 11:01:40 -0700

    filename

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 
-/** main -entry point
-* 
+/**
+* main - entry point
+*
 * Return: 0
 */
 
 int main(void)
 {
-	printf("Print New File %s\n", __FILE__);
+	printf("%s\n", __FILE__);
 
 	return (0);
 }
