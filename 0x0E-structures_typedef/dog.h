b26e61374d3d81060d3072c3d3551bbe6713f815
mkothm <mkothman.jr@gmail.com>
2022-10-10 14:46:16 -0700

    struct and typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,18 @@
+#ifndef FOO_H
+#define FOO_H
+
+/**
+* struct dog - check code
+* @name: first member
+* @age: second member
+* @owner: 3rd member
+*/
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
