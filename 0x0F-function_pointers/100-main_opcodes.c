8a257954e8b2b67f1ea09288b41526a676e5974e
mkothm <mkothman.jr@gmail.com>
2022-10-12 12:54:58 -0700

    new

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -24,10 +24,10 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(2);
 	}
-	for (i = 0; i < nbytes; i++)
+	for (i = 0; i < bytes; i++)
 	{
 		printf("%02x", opc[i] & 0xFF);
-		if (i != nbytes - 1)
+		if (i != bytes - 1)
 			printf(" ");
 	}
 	printf("\n");
