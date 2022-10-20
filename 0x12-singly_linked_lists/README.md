60e7669a5588caabbc464d82798b5c7654c6b9ea
mkothm <mkothman.jr@gmail.com>
2022-10-20 11:15:45 -0700

    prints all element

---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+singly linked lists, a type of linked lists,contains nodes — data and links — koves only in one direction. from head to tail (last node).
---
+++ 0x12-singly_linked_lists/list.h
@@ -0,0 +1,24 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string
+ * @len: length
+ * @next: next node
+ *
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+
+#endif
