c94d2314cc986651b950dbd6fac2399d4ff40a91
mkothm <mkothman.jr@gmail.com>
2023-01-06 10:04:52 -0800

    new

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,3 +1,11 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - Hash fxn implemeting the djb2 algorithm
+ * @str: string
+ * Return: calculated hash
+ */
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
     unsigned long int hash;
